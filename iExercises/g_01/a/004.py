45971c4d698ccb6194642fe3d99c5a3517fd10e7
wahabu <ritscg2hibo@gmail.com>
2024-09-03 21:14:19 +0900

    I successfully completed exercise number 4 of Basic Part 1

--- iExercises/g_01/a/004.py
+++ iExercises/g_01/a/004.py
@@ -1,4 +1,11 @@
-# Write a Python program that calculates the area of a circle based on the radius entered by the user.
-# Sample Output:
-# r = 1.1
-# Area = 3.8013271108436504
+# -----------------------------------------------------------------------------
+# -- Calculates the area of a circle based on the radius entered by the user --
+# -- Sample Output: -----------------------------------------------------------
+# -- r = 1.1 ------------------------------------------------------------------
+# -- Area = 3.8013271108436504 ------------------------------------------------
+import math
+
+python_pi = math.pi
+radius = float(input("What\'s Radius of your circle?"))
+area = python_pi * (radius * radius)
+print(f"Area = {area}")
