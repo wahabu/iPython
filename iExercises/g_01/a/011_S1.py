f47cd575c9fc09bd791270ffc2b2affdb7b2cd28
wahabu <ritscg2hibo@gmail.com>
2024-09-08 23:59:54 +0900

    Implement Python program to print documentation of built-in functions

--- iExercises/g_01/a/011_S1.py
+++ iExercises/g_01/a/011_S1.py
@@ -1,3 +1,12 @@
-s = int(input("Input any numbers: "))
+# -----------------------------------------------------------------------------
+# -- This file demonstrates how to print the documentation (docstring) of various Python built-in functions. Each function's docstring provides a description of its purpose and usage.
+# -----------------------------------------------------------------------------
 
-print(abs(s))
+# Print the docstring (documentation) of the 'abs' function
+print(abs.__doc__) # Return the absolute value of the argument.
+
+# Print the docstring (documentation) of the 'len' function
+print(len.__doc__) # Return the number of items in a container.
+
+# Print the docstring (documentation) of the 'sorted' function
+print(sorted.__doc__)
