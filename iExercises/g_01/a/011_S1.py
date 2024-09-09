b31034fc4775d0d96a76064e31a6ae07e0742e5f
wahabu <ritscg2hibo@gmail.com>
2024-09-09 21:18:46 +0900

    Update Python program to print more documentation of built-in functions

--- iExercises/g_01/a/011_S1.py
+++ iExercises/g_01/a/011_S1.py
@@ -10,3 +10,12 @@ print(len.__doc__) # Return the number of items in a container.
 
 # Print the docstring (documentation) of the 'sorted' function
 print(sorted.__doc__)
+
+# Print the docstring (documentation) of the 'sum' function
+print(sum.__doc__)
+
+# Print the docstring (documentation) of the 'map' function
+print(map.__doc__)
+
+# Print the docstring (documentation) of the 'filter' function
+print(filter.__doc__)
