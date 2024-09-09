628e72edea002141c978606b38d974d5e1a7f369
wahabu <ritscg2hibo@gmail.com>
2024-09-09 21:54:15 +0900

    Create Python program to calculate the volume of a sphere with radius 6

---
+++ iExercises/g_01/a/015_S1.py
@@ -0,0 +1,14 @@
+# -----------------------------------------------------------------------------
+# -- Write a Python program to get the volume of a sphere with radius six. ----
+# -----------------------------------------------------------------------------
+
+# Import the 'pi' constant from the 'math' module to calculate the volume of a sphere
+from math import pi
+
+# Define the radius of the sphere
+r = 6.0
+
+# Calculate the volume of the sphere using the formula
+V = 4.0/3.0 * pi * r**3
+
+print("The volume of the sphere is: ", V)
