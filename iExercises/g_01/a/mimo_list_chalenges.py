d1aaff740c01cdb766fde10a91dd2e88b63693a6
wahabu <ritscg2hibo@gmail.com>
2024-09-10 23:26:30 +0900

    Create Python program to print reminder for each shopping list item using a for loop

---
+++ iExercises/g_01/a/mimo_list_chalenges.py
@@ -0,0 +1,14 @@
+# | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - |
+# | Let's use for loops to keep us on track with our shopping_list list!
+# | Tasks:
+# | 1. For each item, print out a reminder sentence following this format: "Don't forget to buy <...>!".
+# | 2. Replace <...> with the value of the current shopping list item in your loop.
+# | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - | - |
+
+# Define a list called shopping_list with multiple items
+shopping_list = ["dish soap", "kleenex", "batteries", "aluminum foil", "pet food", "toothpaste", "lightbulbs"]
+
+# Loop through each item in the shopping_list
+for i in shopping_list:
+    # Print a reminder sentence for each item in the list
+    print(f"Don't forget to buy {i}!")
