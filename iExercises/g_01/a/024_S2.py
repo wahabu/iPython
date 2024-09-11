d14718b8e6d8cdf0ac66201df5190210991c3e0d
wahabu <ritscg2hibo@gmail.com>
2024-09-12 02:29:36 +0900

    Create another solution Python program to whether a passed letter is a vowel or not.

---
+++ iExercises/g_01/a/024_S2.py
@@ -0,0 +1,15 @@
+# --------------------------------------------------------------------------------------------------
+# - Write a Python to whether a passed letter is a vowel or not.
+# --------------------------------------------------------------------------------------------------
+
+# Define a function called is_vowel that takes one parameter: char (a character).
+def is_vowel(char):
+  # Define a string called all_vowels containing all lowercase vowel characters.
+  all_vowels = 'aeiou'
+  
+  # Check if the input character (char) is present in the all_vowels string.
+  return char in all_vowels
+
+# Call the is_vowel function with two different characters and print the results.
+print("Is a vowel letter? ", is_vowel('c')) # Output: False (character 'c' is not a vowel)
+print("Is a vowel letter? ", is_vowel('e')) # Output: True (character 'e' is a vowel)
