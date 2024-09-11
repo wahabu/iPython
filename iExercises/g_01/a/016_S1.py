4cde5f5b0e2d551bcf2132bd5343011c6cd8ac30
wahabu <ritscg2hibo@gmail.com>
2024-09-11 23:08:05 +0900

    Edit something without change the program

--- iExercises/g_01/a/016_S1.py
+++ iExercises/g_01/a/016_S1.py
@@ -1,6 +1,6 @@
 # ---------------------------------------------------------------------------------------
-# -- Write a Python program to calculate the difference between a given number and 17. --
-# -- If the number is greater than 17, return twice the absolute difference. ------------
+# -- Write a Python program to calculate the difference between a given number and 17. 
+# -- If the number is greater than 17, return twice the absolute difference. 
 # ---------------------------------------------------------------------------------------
 
 # Prompt the user to input the number
