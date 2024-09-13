53e0fa4883f5abc59d113f06678ff92d176bbe83
wahabu <ritscg2hibo@gmail.com>
2024-09-13 22:57:15 +0900

    Edit tesy.py file

--- iExercises/g_01/a/test.py
+++ iExercises/g_01/a/test.py
@@ -30,10 +30,3 @@ print(best)
 print(new)
 print(pace)
 
-# I have a written exam tomorrow.
-# The professor asked us to use a pen for the written part of the exam.
-# I like to sit in the front row so that I can see the board clearly.
-# Hey do you know where the dining hall is?
-# Are you going to the dining hall now?
-# The biology student wore blue gloves to visit her professor's laboratory.
-
