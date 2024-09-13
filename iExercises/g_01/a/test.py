0bcb9c59ddc1b6833ba46ded44c7e23d04cea5a9
wahabu <ritscg2hibo@gmail.com>
2024-09-13 22:55:47 +0900

    Rename 0284_S1.py to 028_S1.py

--- iExercises/g_01/a/test.py
+++ iExercises/g_01/a/test.py
@@ -5,3 +5,35 @@
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
+
+print(7 + 3) # 10
+
+print(3 - 2) # 1
+
+print(7 * 7) # 49
+
+print(9 / 3) # 3
+
+print(11 % 2) # 1
+
+print(8 ** 2) # 64
+
+print(4 // 1) # 4
+print(5 // 2) # 2
+print(7 // 3) # ?
+
+pace = [4.5, 4, 5, 5.2]
+best = pace.pop()
+new = pace.append(6.3)
+
+print(best)
+print(new)
+print(pace)
+
+# I have a written exam tomorrow.
+# The professor asked us to use a pen for the written part of the exam.
+# I like to sit in the front row so that I can see the board clearly.
+# Hey do you know where the dining hall is?
+# Are you going to the dining hall now?
+# The biology student wore blue gloves to visit her professor's laboratory.
+
