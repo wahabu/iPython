071f4faa8d7e60574f89a1c7fd0019acb62b46d9
wahabu <ritscg2hibo@gmail.com>
2024-09-19 23:59:16 +0900

    Add program to convert h (in ft and in) to cm

---
+++ iExercises/g_01/a/059_S1.py
@@ -0,0 +1,8 @@
+# ---------------------------------------------------------------------------------
+# - Write a Python program to convert height (in feet and inches) to centimeters.
+# ---------------------------------------------------------------------------------
+
+height_feet_and_inches = 10
+centimeters = 0
+
+height_centimeters = height_feet_and_inches 
