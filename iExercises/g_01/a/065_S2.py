4738a592562fe79981bcb2be8385c5822ea4ddbe
wahabu <ritscg2hibo@gmail.com>
2024-09-22 23:58:35 +0900

    Add another program to coverts seconds into days, hours, minutes, and seconds.

---
+++ iExercises/g_01/a/065_S2.py
@@ -0,0 +1,3 @@
+# - Write a Python program that converts seconds into days, hours, minutes, and seconds.
+# ----------------------------------------------------------------------------------------
+
