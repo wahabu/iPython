4780656df9da75e801f6fb7fb5dd63ed3a161cf6
wahabu <ritscg2hibo@gmail.com>
2024-09-24 08:19:38 +0900

    Update Q67 Solution1 file.

--- iExercises/g_01/a/067_S1.py
+++ iExercises/g_01/a/067_S1.py
@@ -2,7 +2,7 @@
 # - a millimeter of mercury (mmHg) and atmosphere pressure.
 # ----------------------------------------------------------------------------------------
 
-kPa = int(input("Enter your pressure in kilopascals: "))
+kPa = float(input("Enter your pressure in kilopascals: "))
 
 # pressure in psi = pressure in kPa × 0.1450377.
 psi = kPa * 0.1450377
@@ -13,4 +13,4 @@ atmosphere = psi / 14.6959
 # mmHg value = kPa value x 7.50062
 mmHg = kPa * 7.50062
 
-print(f"PSI = {psi},pmmHg = {mmHg}, and atmosphere = {atmosphere}")
+print("PSI = %.2f,pmmHg = %.2f, and atmosphere = %.2f" % (psi, mmHg, atmosphere))
