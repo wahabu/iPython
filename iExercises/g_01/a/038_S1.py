278359f68c1b7987cb98ccc4f75734901718fecd
wahabu <ritscg2hibo@gmail.com>
2024-09-29 02:27:24 +0900

    Add program to solve '(x + y)^2' with user input.

--- iExercises/g_01/a/038_S1.py
+++ iExercises/g_01/a/038_S1.py
@@ -2,3 +2,13 @@
 # - Test Data: x = 4, y = 3
 # - Expected Output: (4 + 3) ^ 2 = 49
 
+# Prompt the user for input the first integer number
+x = int(input("Input first number: "))
+# Prompt the user for input the second integer number
+y = int(input("Input second number: "))
+
+solve = (x + y ) * (x + y)
+
+print("Test Data: x = %d, y = %d" % (x, y))
+
+print("Expected Output: (%d + %d) ^ 2 = %d" % (x, y, solve))
