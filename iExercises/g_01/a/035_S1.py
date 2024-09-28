7bb5f38b94e6b539ed85b8624f08f6551cd0c559
wahabu <ritscg2hibo@gmail.com>
2024-09-28 17:19:57 +0900

    Add program to check for equality or sum/diff of 5.

--- iExercises/g_01/a/035_S1.py
+++ iExercises/g_01/a/035_S1.py
@@ -6,13 +6,13 @@ x = int(input("Input first number: "))
 # prompt the user for input the second integer number
 y = int(input("Input second number: "))
 
-# Define a function 'check_result_five' that takes two integer inputs: x and y
-def check_result_five(x, y):
+# Define a function 'check_values' that takes two integer inputs: x and y
+def check_values(x, y):
   if x == y or abs(x+y) == 5 or abs(x-y) == 5:
     return True
   else:
     print(abs(x + y))
     return False
 
-# Check the result_five function with different sets of input values and print the results.
-print(check_result_five(x, y))
+# Check the values five function with different sets of input values and print the results.
+print(check_values(x, y))
