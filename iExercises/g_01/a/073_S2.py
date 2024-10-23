9efe46509dfdaa47fe553b54e730b9f32848e54f
wahabu <ritscg2hibo@gmail.com>
2024-10-23 23:58:46 +0900

    Upate program to calculate the midpoints of a line.

--- iExercises/g_01/a/073_S2.py
+++ iExercises/g_01/a/073_S2.py
@@ -1,2 +1,4 @@
 # Write a Python program to calculate the midpoints of a line.
 
+# Print a message to inform the user that the program will calculate the midpoint of a line.
+print('\nCalculate the midpoint of a line: ')
