a77a22f373074d01ab609d100ee4a0108c254db0
wahabu <ritscg2hibo@gmail.com>
2024-10-24 23:32:39 +0900

    Upate program to calculate the midpoints of a line.

--- iExercises/g_01/a/073_S2.py
+++ iExercises/g_01/a/073_S2.py
@@ -2,3 +2,19 @@
 
 # Print a message to inform the user that the program will calculate the midpoint of a line.
 print('\nCalculate the midpoint of a line: ')
+
+# Prompt the user to enter the x and y coordinates of the first endpoint of the line.
+x1 = float(input())
+y1 = float(input())
+
+# Prompt the user to enter the x and y coordinates of the second endpoint of the line.
+x2 = float(input('The value of x (the first endpoint)'))
+y2 = float(input('The value fo y (the first endpoint)'))
+
+# Calculate the x-coordinate of the midpoint of the line.
+x_m_point = (x1 + x2) / 2
+
+# Calculate the y-coordinate of the midpoint of the line.
+y_m_point = (y1 + y2) / 2
+print();
+
