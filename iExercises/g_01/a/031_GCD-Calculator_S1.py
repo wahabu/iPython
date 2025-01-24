bd175d18b86c92e1b7252b73056c1501cfc35dc6
wahabu <ritscg2hibo@gmail.com>
2025-01-24 19:12:57 +0900

    Add program to...

---
+++ iExercises/g_01/a/031_GCD-Calculator_S1.py
@@ -0,0 +1,41 @@
+# ---------------------------------------------------------------------------------------------------
+# - Write a Python program that computes the greatest common divisor (GCD) of two positive integers.
+# ---------------------------------------------------------------------------------------------------
+
+num1 = int(input("Enter a first number: "))
+num2 = int(input("Enter a secound number: "))
+
+list_1 = []
+list_2 = []
+greatest_common_divisor = []
+
+count = 1
+while (count <= num1):
+  if (num1 % count == 0):
+    list_1.append(count)
+  count = count + 1
+
+count = 1
+while (count <= num2):
+  if (num2 % count == 0):
+    list_2.append(count)
+  count = count + 1
+
+greatest_common_divisor = max([item for item in list_1 if item in list_2])
+GCD = []
+for item in list_1:
+  if item in list_2:
+    GCD.append(item)
+    
+GCF = []
+for item in list_1:
+  if item in list_2:
+    GCF += [item]
+
+print(list_1)
+print(list_2)
+# print(max(list_2))
+# print(max(list_1))
+print(greatest_common_divisor)
+print(GCD)
+print(GCF)
