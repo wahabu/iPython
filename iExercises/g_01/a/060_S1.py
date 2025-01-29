c587d93dd27cc837e2e3feaaced3571a31d3caa0
wahabu <ritscg2hibo@gmail.com>
2025-01-29 23:53:32 +0900

    Add program to...

--- iExercises/g_01/a/060_S1.py
+++
@@ -1,9 +0,0 @@
-# - Write a Python program to calculate the hypotenuse of a right angled triangle.
-
-import math
-a = int(input("Enter the side right size: "))
-b = int(input("Enter the side right size: "))
-
-c = math.sqrt(a ** 2 + b ** 2)
-
-print(int(c))
