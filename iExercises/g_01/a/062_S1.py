cf729234d11701e1ea318c988009620685457f39
wahabu <ritscg2hibo@gmail.com>
2025-01-31 21:13:19 +0900

    Deleted old file...

--- iExercises/g_01/a/062_S1.py
+++
@@ -1,9 +0,0 @@
-# - Write a Python program to convert all units of time into seconds.
-# ---------------------------------------------------------------------
-
-h = int(input("Input value of hours: "))
-m = int(input("Input value of minutes: "))
-s = int(input("Input value of seconds: "))
-seconds = ((h*3600+1) + (m*60+1) + s) - 2
-
-print(seconds)
