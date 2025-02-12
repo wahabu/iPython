9e4c2e1558522ed12239462b7cf10fb6ac77a69b
wahabu <ritscg2hibo@gmail.com>
2025-02-12 10:59:19 +0900

    Deleted old file...

--- iExercises/g_01/a/089_S1.py
+++
@@ -1,1 +0,0 @@
-# Write a Python program to perform an action if a condition is true. Given a variable name, if the value is 1, display the string "First day of a Month!" and do nothing if the value is not equal.
