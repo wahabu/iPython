5845f68e49b88d4555dad3256d5c909f4ace4b55
wahabu <ritscg2hibo@gmail.com>
2025-02-28 21:55:11 +0900

    Rename old file...

---
+++ iExercises/g_01/a/024_VowelTester_S3.py
@@ -0,0 +1,16 @@
+# ------------------------------------------------------------------------------
+# - Write a Python program to test whether a passed letter is a vowel or not.
+# ------------------------------------------------------------------------------
+
+# Function to check whether a character
+# is vowel or not
+def vowelOrConsonant(x):
+  
+  if (x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u'):
+    print("Vowel")
+  else:
+    print("Consonant")
+    
+# Driver code
+vowelOrConsonant('c')
+vowelOrConsonant('e')
