d9b5c4a68703d3a71b27a40919e205e48dfddcf6
wahabu <ritscg2hibo@gmail.com>
2025-03-21 01:42:33 +0900

    Add another one solution

---
+++ iExercises/g_02/GetSystemTime_S2.py
@@ -0,0 +1,3 @@
+# Write a Python program to get system time.
+
+# Note : The system time is important for debugging, network information, random number seeds, or something as simple as program performance.
