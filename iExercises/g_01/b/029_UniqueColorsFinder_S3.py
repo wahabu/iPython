0e75ba99c468478f79c5f40fe912d245f37b1d2f
wahabu <ritscg2hibo@gmail.com>
2025-06-06 23:57:52 +0900

    Update file contents

---
+++ iExercises/g_01/b/029_UniqueColorsFinder_S3.py
@@ -0,0 +1,6 @@
+# Write a Python program that prints out all colors from color_list_1 that are not present in color_list_2.
+# Test Data :
+# color_list_1 = set(["White", "Black", "Red"])
+# color_list_2 = set(["Red", "Green"])
+# Expected Output :
+# {'Black', 'White'}
